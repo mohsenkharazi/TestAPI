3ee78ac4ca41209d1f81a1835b12f3f9627c7a09
Mohsen Kharazi <mohsenkharazi@yahoo.com>
2020-09-29 01:02:22 +0800

    nothing special

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    [Route("default")]
+    public class DefaultController : Controller
+    {
+        //[HttpGet]
+        public string Index()
+        {
+            string myString = "Hi";
+         
+            return myString;
+        }
+    }
+}
