3d6bef0ae6148853810a18241d279b7b3943f935
Mohsen Kharazi <mohsenkharazi@yahoo.com>
2020-10-11 15:55:53 +0800

    learning path 1

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -6,15 +6,32 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TestAPI.Controllers
 {
-    [Route("default")]
+    [ApiController]
+    [Route("[controller]")]
+    //[Route("default")]
     public class DefaultController : Controller
     {
         //[HttpGet]
         public string Index()
         {
-            string myString = "Hi";
+            string myString = null;
+
+            // testting verbatim string
+            myString = @"c:\user\mohsen";
+
+            //testing nullable data types
+            int? myVar = null;
+            myString = myVar.ToString();
+
+            //data type conversion
+            float myFloatVar = 123.45F;
+            int myIntVar = (int)myFloatVar;
+
+            //testing arrays --> arrays are not type safe you may point to unavailable index of arrayear
+            string[] myArrVar = new string[] { "Mohsen", "Unknown"};
+            
          
-            return myString;
+            return myArrVar[4];
         }
     }
 }
